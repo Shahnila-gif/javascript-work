cf21c9b540333bb723a9eb5378e308a2f862a77c
Shahnila-gif <abihablouch28@gmail.com>
2024-09-19 10:25:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// Qno:01
+var name="Shahnila"
+alert(name);
+
+// Qno:02
+var fullname="Shahnila Abid"
+console.log ("my name is" +" "+fullname);
+
+
+// Qno:03
+var a = 4
+var b = 2
+console.log (a++ + --b - --a + b++ + a + b);
+
+// Qno:04
+
+var Qno1 = prompt("are you a student at SMIT?");
+var Qno2 = prompt("is your attendence is complete?");
+if (Qno1 === "yes" && Qno2 === "yes"){
+    alert("you are able to attend the class"); 
+}
+else if ( Qno2 === "no" && Qno2==="no"){
+    alert("you are not able to attend the class");
+}
